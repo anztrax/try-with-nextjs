c9e3aacfc5525ee20a892961e88a28da9d64db16
andrew ananta <kuro.ketanitem@gmail.com>
2019-01-22 23:46:56 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Hello there</h1>
+      </div>
+    )
+  }
+}
+
+export default MainPage;
